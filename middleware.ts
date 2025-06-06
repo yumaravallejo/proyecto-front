00b6075e5caf272b7695c1aebbedc229b61ac4ea
Yumara Vallejo <145961485+yumaravallejo@users.noreply.github.com>
2025-06-06 02:25:36 +0200

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,23 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-  const { pathname } = req.nextUrl;
+  let pathname = req.nextUrl.pathname;
+  console.log("Middleware - Pathname:", pathname);
+  console.log("Middleware - Token cookie:", token);
+
+  // quitar slash final para evitar problemas
+  pathname = pathname.endsWith("/") && pathname.length > 1 ? pathname.slice(0, -1) : pathname;
 
   const publicRoutes = ["/", "/login", "/registro", "/cuotas", "/servicios", "/actividades"];
   const privateRoutes = ["/dashboard", "/perfil", "/dietas", "/calendario", "/reservas", "/horarios-actividades", "/administracion"];
 
   if (token && publicRoutes.includes(pathname)) {
+    console.log("Redirigiendo a /dashboard porque ya está autenticado");
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   if (!token && privateRoutes.includes(pathname)) {
+    console.log("Redirigiendo a / porque no está autenticado");
     return NextResponse.redirect(new URL("/", req.url));
   }
 
