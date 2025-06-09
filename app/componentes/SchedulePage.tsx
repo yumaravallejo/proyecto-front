f03cbdee99d1f8bd2d9ec1957cb6ee082c6ed1a0
Yumara Vallejo <145961485+yumaravallejo@users.noreply.github.com>
2025-06-09 12:34:37 +0200

    updated

--- app/componentes/SchedulePage.tsx
+++ app/componentes/SchedulePage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { JSX, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast, Toaster } from "sonner";
 
 export type TipoClase =
