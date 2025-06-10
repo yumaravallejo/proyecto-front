6975db208bb2206272854ce7b96baf2be90edc26
Yumara Vallejo <145961485+yumaravallejo@users.noreply.github.com>
2025-06-10 10:58:14 +0200

    added shities

--- app/componentes/Header.tsx
+++ app/componentes/Header.tsx
@@ -11,12 +11,12 @@ type Props = {
 };
 
 interface Usuario {
-  id: number,
-  token: string,
-  tipo: string,
-  imagen: string,
-  email: string,
-  nombre: string
+  id: number;
+  token: string;
+  tipo: string;
+  imagen: string;
+  email: string;
+  nombre: string;
 }
 
 export default function Header(props: Props) {
@@ -57,37 +57,48 @@ export default function Header(props: Props) {
   if (!isClient) return null;
 
   const login = !!user;
-  const imagenUser = user?.imagen ?? "/usuario.svg";
+  const imagenUser =
+    user?.imagen == ""
+      ? "/usuario.svg"
+      : `${process.env.NEXT_PUBLIC_API}/usuarios/obtenerArchivo?imagen=${user?.imagen}`;
 
-  const opcionesMenu = login ? (
-    user.tipo !== "Entrenador" ? [
-      { nombre: "DIETAS", ruta: "/dietas" },
-      { nombre: "CALENDARIO", ruta: "/calendario" },
-      { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
-      { nombre: "CONTACTO", ruta: "/contacto" },
-    ] : [
-      { nombre: "DIETAS", ruta: "/dietas" },
-      { nombre: "CALENDARIO", ruta: "/calendario" },
-      { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
-      { nombre: "ADMINISTRACIÓN", ruta: "/administracion" },
-    ]) : [
-    { nombre: "CUOTAS", ruta: "/cuotas" },
-    { nombre: "SERVICIOS", ruta: "/servicios" },
-    { nombre: "ACTIVIDADES", ruta: "/actividades" },
-    { nombre: "CONTACTO", ruta: "/contacto" },
-    { nombre: "ÚNETE", ruta: "/registro" },
-  ];
+  const opcionesMenu = login
+    ? user.tipo !== "Entrenador"
+      ? [
+          { nombre: "DIETAS", ruta: "/dietas" },
+          { nombre: "CALENDARIO", ruta: "/calendario" },
+          { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
+          { nombre: "CONTACTO", ruta: "/contacto" },
+        ]
+      : [
+          { nombre: "DIETAS", ruta: "/dietas" },
+          { nombre: "CALENDARIO", ruta: "/calendario" },
+          { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
+          { nombre: "ADMINISTRACIÓN", ruta: "/administracion" },
+        ]
+    : [
+        { nombre: "CUOTAS", ruta: "/cuotas" },
+        { nombre: "SERVICIOS", ruta: "/servicios" },
+        { nombre: "ACTIVIDADES", ruta: "/actividades" },
+        { nombre: "CONTACTO", ruta: "/contacto" },
+        { nombre: "ÚNETE", ruta: "/registro" },
+      ];
 
   return (
     <>
       <header
-        className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 gap-x-15 transition-all duration-300 z-50 ${isScrolled ? "shadow-lg bg-[var(--gris-oscuro)]" : "bg-[var(--gris-oscuro)]"
-          }`}
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 gap-x-15 transition-all duration-300 z-50 ${
+          isScrolled
+            ? "shadow-lg bg-[var(--gris-oscuro)]"
+            : "bg-[var(--gris-oscuro)]"
+        }`}
         style={{ height: HEADER_HEIGHT }}
       >
         {/* Botón menú */}
         <label
-          className={`menu-btn cursor-pointer ${menuAbierto ? "menu-abierto" : "menu-cerrado"}`}
+          className={`menu-btn cursor-pointer ${
+            menuAbierto ? "menu-abierto" : "menu-cerrado"
+          }`}
           onClick={() => setMenuAbierto(!menuAbierto)}
         >
           <span></span>
@@ -97,7 +108,13 @@ export default function Header(props: Props) {
 
         {/* Logo */}
         <Link href="/" className="flex items-center gap-4">
-          <Image src="/logo.svg" alt="Logo de Changes" width={80} height={50} className="rounded-full" />
+          <Image
+            src="/logo.svg"
+            alt="Logo de Changes"
+            width={80}
+            height={50}
+            className="rounded-full"
+          />
           <span className="text-xl font-bold text-white flex flex-col">
             <span className="amarillo font-oswald">CHANGES</span>
             <span className="azul oswald">FITNESS CLUB</span>
@@ -111,17 +128,36 @@ export default function Header(props: Props) {
               key={index}
               href={opcion.ruta}
               className={`text-md font-bold text-white hover:text-[var(--azul)] transition-colors duration-200
-                ${opcion.nombre === (props.pagina ?? "").toUpperCase() ? "border-b-4 border-[var(--azul)]" : ""}`}
+                ${
+                  opcion.nombre === (props.pagina ?? "").toUpperCase()
+                    ? "border-b-4 border-[var(--azul)]"
+                    : ""
+                }`}
             >
               {opcion.nombre}
             </Link>
           ))}
           {/* Redes sociales */}
           <nav id="redes" className="flex gap-4 absolute bottom-30">
-            <Image src="/instagram-am.svg" alt="Instagram" width={40} height={40} />
+            <Image
+              src="/instagram-am.svg"
+              alt="Instagram"
+              width={40}
+              height={40}
+            />
             <Image src="/tiktok-am.svg" alt="Tiktok" width={40} height={40} />
-            <Image src="/whatsapp-am.svg" alt="Whatsapp" width={40} height={40} />
-            <Image src="/facebook-am.svg" alt="Facebook" width={40} height={40} />
+            <Image
+              src="/whatsapp-am.svg"
+              alt="Whatsapp"
+              width={40}
+              height={40}
+            />
+            <Image
+              src="/facebook-am.svg"
+              alt="Facebook"
+              width={40}
+              height={40}
+            />
           </nav>
         </nav>
 
@@ -130,7 +166,9 @@ export default function Header(props: Props) {
           <div className="perfil-ic flex justify-end mr-6">
             <Link
               href="/perfil"
-              className={`${imagenUser?.trim() ? "avatar" : "no-avatar"} sesion logged flex rounded-full`}
+              className={`${
+                imagenUser?.trim() ? "avatar" : "no-avatar"
+              } sesion logged flex rounded-full`}
               title="Ir al perfil"
             >
               {imagenUser?.trim() ? (
@@ -142,7 +180,13 @@ export default function Header(props: Props) {
                   />
                 </Avatar>
               ) : (
-                <Image src="/usuario.svg" alt="Usuario" width={50} height={50} className="usuario" />
+                <Image
+                  src="/usuario.svg"
+                  alt="Usuario"
+                  width={50}
+                  height={50}
+                  className="usuario"
+                />
               )}
             </Link>
           </div>
--- app/componentes/HeaderUs.tsx
+++ app/componentes/HeaderUs.tsx
@@ -11,12 +11,12 @@ type Props = {
 };
 
 interface Usuario {
-  id: number,
-  token: string,
-  tipo: string,
-  imagen: string,
-  email: string,
-  nombre: string
+  id: number;
+  token: string;
+  tipo: string;
+  imagen: string;
+  email: string;
+  nombre: string;
 }
 
 export default function HeaderUs(props: Props) {
@@ -31,7 +31,10 @@ export default function HeaderUs(props: Props) {
   }, []);
 
   const login = !!user;
-  const imagenUser = user?.imagen ?? "/usuario.svg";
+  const imagenUser =
+    user?.imagen == ""
+      ? "/usuario.svg"
+      : `${process.env.NEXT_PUBLIC_API}/usuarios/obtenerArchivo?imagen=${user?.imagen}`;
 
   useEffect(() => {
     function actualizarUser() {
@@ -72,7 +75,9 @@ export default function HeaderUs(props: Props) {
       <>
         <header className="flex items-center justify-between gap-x-15 w-full bg-[var(--gris-oscuro)] text-white transition-all duration-300 z-50">
           <label
-            className={`${menuAbierto ? "menu-abierto" : "menu-cerrado"} menu-btn cursor-pointer ml-7`}
+            className={`${
+              menuAbierto ? "menu-abierto" : "menu-cerrado"
+            } menu-btn cursor-pointer ml-7`}
             onClick={() => {
               if (!menuAbierto) {
                 window.scrollTo({ top: 0, behavior: "auto" });
@@ -86,7 +91,13 @@ export default function HeaderUs(props: Props) {
           </label>
 
           <Link href="/" className="flex items-center gap-4">
-            <Image src="/logo.svg" alt="Logo de Changes" width={80} height={50} className="rounded-full" />
+            <Image
+              src="/logo.svg"
+              alt="Logo de Changes"
+              width={80}
+              height={50}
+              className="rounded-full"
+            />
             <span className="text-xl font-bold text-white flex flex-col">
               <span className="amarillo oswald">CHANGES</span>
               <span className="azul oswald">FITNESS CLUB</span>
@@ -99,16 +110,35 @@ export default function HeaderUs(props: Props) {
                 key={index}
                 href={opcion.ruta}
                 className={`text-md font-bold text-white hover:text-[var(--azul)] transition-colors duration-200 px-2
-          ${opcion.nombre === (props.pagina ?? "").toUpperCase() ? "border-b-4 border-[var(--azul)]" : ""}`}
+          ${
+            opcion.nombre === (props.pagina ?? "").toUpperCase()
+              ? "border-b-4 border-[var(--azul)]"
+              : ""
+          }`}
               >
                 {opcion.nombre}
               </Link>
             ))}
             <nav id="redes" className="flex gap-4 absolute bottom-30">
-              <Image src="/instagram-am.svg" alt="Instagram" width={40} height={40} />
+              <Image
+                src="/instagram-am.svg"
+                alt="Instagram"
+                width={40}
+                height={40}
+              />
               <Image src="/tiktok-am.svg" alt="Tiktok" width={40} height={40} />
-              <Image src="/whatsapp-am.svg" alt="Whatsapp" width={40} height={40} />
-              <Image src="/facebook-am.svg" alt="Facebook" width={40} height={40} />
+              <Image
+                src="/whatsapp-am.svg"
+                alt="Whatsapp"
+                width={40}
+                height={40}
+              />
+              <Image
+                src="/facebook-am.svg"
+                alt="Facebook"
+                width={40}
+                height={40}
+              />
             </nav>
           </nav>
 
@@ -130,30 +160,35 @@ export default function HeaderUs(props: Props) {
   }
 
   // Opciones de menú para usuario logueado o no
-  const opcionesMenu = login ? (
-    user.tipo !== "Entrenador" ? [
-      { nombre: "DIETAS", ruta: "/dietas" },
-      { nombre: "CALENDARIO", ruta: "/calendario" },
-      { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
-      { nombre: "CONTACTO", ruta: "/contacto" },
-    ] : [
-      { nombre: "DIETAS", ruta: "/dietas" },
-      { nombre: "CALENDARIO", ruta: "/calendario" },
-      { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
-      { nombre: "ADMINISTRACIÓN", ruta: "/administracion" },
-    ]) : [
-    { nombre: "CUOTAS", ruta: "/cuotas" },
-    { nombre: "SERVICIOS", ruta: "/servicios" },
-    { nombre: "ACTIVIDADES", ruta: "/actividades" },
-    { nombre: "CONTACTO", ruta: "/contacto" },
-    { nombre: "ÚNETE", ruta: "/registro" },
-  ];
+  const opcionesMenu = login
+    ? user.tipo !== "Entrenador"
+      ? [
+          { nombre: "DIETAS", ruta: "/dietas" },
+          { nombre: "CALENDARIO", ruta: "/calendario" },
+          { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
+          { nombre: "CONTACTO", ruta: "/contacto" },
+        ]
+      : [
+          { nombre: "DIETAS", ruta: "/dietas" },
+          { nombre: "CALENDARIO", ruta: "/calendario" },
+          { nombre: "ACTIVIDADES", ruta: "/horarios-actividades" },
+          { nombre: "ADMINISTRACIÓN", ruta: "/administracion" },
+        ]
+    : [
+        { nombre: "CUOTAS", ruta: "/cuotas" },
+        { nombre: "SERVICIOS", ruta: "/servicios" },
+        { nombre: "ACTIVIDADES", ruta: "/actividades" },
+        { nombre: "CONTACTO", ruta: "/contacto" },
+        { nombre: "ÚNETE", ruta: "/registro" },
+      ];
 
   return (
     <>
       <header className="flex items-center justify-between gap-x-15 w-full bg-[var(--gris-oscuro)] text-white transition-all duration-300 z-50">
         <label
-          className={`${menuAbierto ? "menu-abierto" : "menu-cerrado"} menu-btn cursor-pointer ml-7`}
+          className={`${
+            menuAbierto ? "menu-abierto" : "menu-cerrado"
+          } menu-btn cursor-pointer ml-7`}
           onClick={() => {
             if (!menuAbierto) {
               window.scrollTo({ top: 0, behavior: "auto" });
@@ -167,7 +202,13 @@ export default function HeaderUs(props: Props) {
         </label>
 
         <Link href="/" className="flex items-center gap-4">
-          <Image src="/logo.svg" alt="Logo de Changes" width={80} height={50} className="rounded-full" />
+          <Image
+            src="/logo.svg"
+            alt="Logo de Changes"
+            width={80}
+            height={50}
+            className="rounded-full"
+          />
           <span className="text-xl font-bold text-white flex flex-col">
             <span className="amarillo oswald">CHANGES</span>
             <span className="azul oswald">FITNESS CLUB</span>
@@ -180,16 +221,35 @@ export default function HeaderUs(props: Props) {
               key={index}
               href={opcion.ruta}
               className={`text-md font-bold text-white hover:text-[var(--azul)] transition-colors duration-200 px-2
-        ${opcion.nombre === (props.pagina ?? "").toUpperCase() ? "border-b-4 border-[var(--azul)]" : ""}`}
+        ${
+          opcion.nombre === (props.pagina ?? "").toUpperCase()
+            ? "border-b-4 border-[var(--azul)]"
+            : ""
+        }`}
             >
               {opcion.nombre}
             </Link>
           ))}
           <nav id="redes" className="flex gap-4 absolute bottom-30">
-            <Image src="/instagram-am.svg" alt="Instagram" width={40} height={40} />
+            <Image
+              src="/instagram-am.svg"
+              alt="Instagram"
+              width={40}
+              height={40}
+            />
             <Image src="/tiktok-am.svg" alt="Tiktok" width={40} height={40} />
-            <Image src="/whatsapp-am.svg" alt="Whatsapp" width={40} height={40} />
-            <Image src="/facebook-am.svg" alt="Facebook" width={40} height={40} />
+            <Image
+              src="/whatsapp-am.svg"
+              alt="Whatsapp"
+              width={40}
+              height={40}
+            />
+            <Image
+              src="/facebook-am.svg"
+              alt="Facebook"
+              width={40}
+              height={40}
+            />
           </nav>
         </nav>
 
@@ -197,7 +257,9 @@ export default function HeaderUs(props: Props) {
           <div className="perfil-ic flex justify-end mr-6">
             <Link
               href="/perfil"
-              className={`${imagenUser.trim() ? "avatar" : "no-avatar"} sesion logged flex rounded-full`}
+              className={`${
+                imagenUser.trim() ? "avatar" : "no-avatar"
+              } sesion logged flex rounded-full`}
               title="Ir al perfil"
             >
               {imagenUser.trim() ? (
@@ -209,7 +271,13 @@ export default function HeaderUs(props: Props) {
                   />
                 </Avatar>
               ) : (
-                <Image src="/usuario.svg" alt="Usuario" width={50} height={50} className="usuario" />
+                <Image
+                  src="/usuario.svg"
+                  alt="Usuario"
+                  width={50}
+                  height={50}
+                  className="usuario"
+                />
               )}
             </Link>
           </div>
